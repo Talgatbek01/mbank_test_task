fb477578bc091b9da035294068f0ed5931a4b5db
Talgat <121433632+Talgatbek01@users.noreply.github.com>
2025-08-20 18:12:41 +0600

    deleted header from network_module

--- lib/core/network_module/network_module.dart
+++ lib/core/network_module/network_module.dart
@@ -9,18 +9,12 @@ import '../interceptor/interceptor.dart';
 abstract class NetworkModule {
   @lazySingleton
   Dio dio() {
-    const Map<String, dynamic> headers = {
-      'Content-Type': 'application/json',
-      'Accept': 'application/json',
-    };
-
     final dio = Dio(
       BaseOptions(
         baseUrl: Constants.baseUrl,
         connectTimeout: const Duration(seconds: 15),
         receiveTimeout: const Duration(seconds: 15),
         sendTimeout: const Duration(seconds: 15),
-        headers: headers,
       ),
     );
     dio.interceptors.add(DioInterceptor());
